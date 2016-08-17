c99ce8637a4f62ae5e355035f2c8582fca89cde2
filecon <coach.samf@gmail.com>
2016-08-17 12:13:50 +1000

    Updated program
    Completed program and functioning on both activity’s.

--- app/src/main/java/com/example/sam/week4intents/ActivityB.java
+++ app/src/main/java/com/example/sam/week4intents/ActivityB.java
@@ -7,12 +7,12 @@ import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
 
-public class ActivityA extends AppCompatActivity {
+public class ActivityB extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_a);
+        setContentView(R.layout.activity_b);
 
         // Retrieving values
         Bundle extras = getIntent().getExtras();
@@ -22,14 +22,15 @@ public class ActivityA extends AppCompatActivity {
         String qString = extras.getString("qString"); // get value passed
         TextView textView = (TextView) findViewById(R.id.textView1); // set up variable for screen field
         textView.setText(qString); //populate screen variable
-
     }
+
     public void onClick (View view) {
-        Intent i = new Intent(this, ActivityB.class);
+        Intent i = new Intent(this, ActivityA.class);
+
         //Passing Values
         EditText editText = (EditText) findViewById(R.id.editText1); //create variable for screen input
         String myString = editText.getText().toString(); //move value to myString
-        i.putExtra("qString", myString); // Pass value to ActivityB
+        i.putExtra("qString", myString); // Pass value to ActivityA
 
         startActivity(i);
     }
